18199b3682f44a1ea8d94bc9234d7719e4cf883d
Sohaib-GO <s.almajmaie@gmail.com>
2023-01-23 19:29:40 -0700

    Added new stores and fetched subset of stores based on apparel and annual revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,26 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Store.create(name: "Surrey", annual_revenue: 224000, womens_apparel: true)
+Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true)
+Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+# Fetch stores that carry men's apparel
+@mens_stores = Store.where(mens_apparel: true)
+
+# Output name and annual revenue for each store
+@mens_stores.each do |store|
+  puts "Store Name: #{store.name}"
+  puts "Annual Revenue: #{store.annual_revenue}"
+end
+
+# Fetch stores
+puts "Stores that has women apparel and annual revenue less than 1M"
+@womens_stores = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+
+# Output name and annual revenue for each store
+@womens_stores.each do |store|
+  puts "Store Name: #{store.name}"
+  puts "Annual Revenue: #{store.annual_revenue}"
+end
+
