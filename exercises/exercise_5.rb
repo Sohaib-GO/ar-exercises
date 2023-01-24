85488d63095e6f6b5c649d97cd6b55cf8e44d18c
Sohaib-GO <s.almajmaie@gmail.com>
2023-01-23 19:33:22 -0700

    Implemented calculations for total revenue, average annual revenue, and number of stores generating M or more in annual sales

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: #{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Revenue: #{average_revenue}"
+
+puts "Number of stores with annual revenue greater than 1M: #{Store.where("annual_revenue > ?", 1000000).count}"
