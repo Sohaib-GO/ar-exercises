9720c0de3696771c886d1beb536851a19c8d472a
Sohaib-GO <s.almajmaie@gmail.com>
2023-01-23 19:58:37 -0700

    Add validations to models and user input for creating a store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,15 +5,30 @@ puts "----------"
 
 class Store < ActiveRecord::Base 
   has_many :employees
+
+  validates :name, presence: true, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_carry_apparel 
+
+  def must_carry_apparel
+    if !mens_apparel && !womens_apparel
+    errors.add(:base, "Store must carry at least one type of apparel (men's or women's)")
+    end 
+  end 
 end
 
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+
 end 
 
 Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-
 puts Store.count
 
